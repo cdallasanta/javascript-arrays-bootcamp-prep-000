ed2a99477ca500abe7347cb6bcee8501a8623c87
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-11-17 20:03:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,10 @@
 let chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
+function addElementToBeginningOfArray(arr, elem){
+  return [elem, ...arr];
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, elem){
+  arr.unshift(elem);
+  return arr;
+}
