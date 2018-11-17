30e45e4a04becf17378b35c6e7d82c98d0f0c7da
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-11-17 20:05:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,12 @@ function destructivelyAddElementToBeginningOfArray(arr, elem){
   arr.unshift(elem);
   return arr;
 }
+
+function addElementToEndOfArray(arr, elem){
+  return [...arr,elem];
+}
+
+function destructivelyAddElementToEndOfArray(arr, elem){
+  arr.push(elem);
+  return arr;
+}
